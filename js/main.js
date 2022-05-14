b77f8c9a716f9ee4577e02f2f8eaf2badab84395
quocky1605 <83623156+quocky1605@users.noreply.github.com>
2022-05-14 16:31:54 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+
+    // $('.search-icon').click(function (e) {
+    //     e.preventDefault()
+    //     $('input.searchInput').addClass('activeInput')
+    //     $('input.searchInput').focus()
+    // })
+
+
+});
